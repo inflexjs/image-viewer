cd1c2802dc7f3ff8ceb1bf70cf4e794df902bb5f
inflex <inflex_off@vk.com>
2022-08-30 18:13:53 +0300

    public path for gh-p

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+	publicPath: '/image-viewer/',
 	transpileDependencies: true,
 	chainWebpack: config => {
 		config.module
